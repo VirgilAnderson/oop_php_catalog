bf07869b6a40790501c7481c51cebc861a635a3b
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 13:03:17 -0700

    Feat: Added main section on index.php

--- magic_classified/public/index.php
+++ magic_classified/public/index.php
@@ -2,5 +2,15 @@
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
+<main>
+  <section id='main_menu'>
+    <ul id='menu'>
+      <li><a href=''>About Us</a></li>
+      <li><a href=''>View Listings</a></li>
+    </ul>
+  </section>
+</main>
+
+
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
