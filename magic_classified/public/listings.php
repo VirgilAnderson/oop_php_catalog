17b4cd87c09b30ac3d919c7d7809fcc0fe0a2315
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 14:31:50 -0700

    Feat: Added sidebar.php

--- magic_classified/public/listings.php
+++ magic_classified/public/listings.php
@@ -2,10 +2,17 @@
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
-<main>
-  
-</main>
+<main class='row'>
+  <aside class='column'><?php include(SHARED_PATH . '/sidebar.php'); ?></aside>
 
+  <article class='column listings'>
+      <h1>Newest Listings</h2>
+      <div></div>
+  </article>
+
+  <aside class='column'>Column</aside>
+
+</main>
 
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
