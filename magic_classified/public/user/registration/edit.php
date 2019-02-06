0c6676bd8d4f096133ea1af5e837c6bf421efca5
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 19:31:34 -0700

    Feat: Added form_fields.php

--- magic_classified/public/user/registration/edit.php
+++ magic_classified/public/user/registration/edit.php
@@ -2,7 +2,34 @@
 <?php $page_title = 'Edit My Account'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
-<main>
+<main class='row'>
+  <aside class='column'><?php include(SHARED_PATH . '/sidebar.php'); ?></aside>
+
+  <article class='column listings'>
+      <div class="listing_details">
+        <div class='listing_title'>
+          <h2>Edit My Account</h2>
+        </div><!-- .listing_title -->
+
+        <div class='listing_body'>
+          <div class='listing_info'>
+            <form>
+              <?php include('form_fields.php'); ?>
+              <input type='submit' value='edit account' />
+            </form>
+          </div><!-- .listing_info -->
+
+        </div><!--listing_body -->
+        <div class='listing_footer'>
+          <ul class='footer_menu'>
+            <li><a href='<?php echo url_for('/user/registration/index.php'); ?>'>My Account</a></li>
+            <li><a href='<?php echo url_for('/user/registration/delete.php'); ?>'>Delete Account</a></li>
+          </ul>
+        </div><!-- listing_footer -->
+      </div><!-- listing_details -->
+  </article>
+
+  <aside class='column'>Column</aside>
 
 </main>
 
---
+++ magic_classified/public/user/registration/form_fields.php
@@ -0,0 +1,19 @@
+<dl>
+  <dt><label for='username'>Username:</label></dt>
+  <dd><input type="text" name="username" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='email'>Email Address:</label></dt>
+  <dd><input type="text" name="email" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='password'>Password:</label></dt>
+  <dd><input type="text" name="password" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='confirm_password'>Confirm Password:</label></dt>
+  <dd><input type="text" name="confirm_password" value="" /></dd>
+</dl>
