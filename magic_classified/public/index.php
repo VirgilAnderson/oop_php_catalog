339ab1a0d32dd21374e52896988165a5fbfc847a
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-06 12:05:39 -0700

    Feat: Adding responsive elements

--- magic_classified/public/index.php
+++ magic_classified/public/index.php
@@ -3,12 +3,21 @@
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <main>
+  <section class='main_section'>
+    <div class='direct'>
+      <a href='<?php echo url_for('/user/registration/index.php'); ?>'><i class="far fa-user-circle"></i> Account</a>
+    </div>
+    <div class='direct'>
+      <a href='<?php echo url_for('/listings.php'); ?>'><i class="fas fa-frog"></i> Listings</a>
+    </div>
+  </section>
+ <!--
   <section id='main_menu'>
     <ul id='menu'>
       <li><a href='<?php echo url_for('/user/registration/index.php'); ?>'><i class="far fa-user-circle"></i> My Account</a></li>
       <li><a href='<?php echo url_for('/listings.php'); ?>'><i class="fas fa-frog"></i> Listings</a></li>
     </ul>
-  </section>
+  </section> -->
 </main>
 
 
