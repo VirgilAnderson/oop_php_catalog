ee1d076f1db48c3097ed1eea18fccb648ee2085d
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 19:57:25 -0700

    Feat: Added account_listings/edit.php content

---
+++ magic_classified/public/user/account_listings/form_fields.php
@@ -0,0 +1,48 @@
+<dl>
+  <dt><label for='name'>Name:</label></dt>
+  <dd><input type="text" name="name" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='description'>Description:</label></dt>
+  <dd><input type="text" name="description" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='category'>Category:</label></dt>
+  <dd>
+    <select name="category">
+      <option value='closeup'>Close Up Magic</option>
+      <option value='stage'>Stage Magic</option>
+      <option value='illusion'>Stage Illusion</option>
+    </select>
+  </dd>
+</dl>
+
+<dl>
+  <dt><label for='price'>Price:</label></dt>
+  <dd><input type="text" name="price" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='manufacturer'>Manufacturer:</label></dt>
+  <dd><input type="text" name="manufacturer" value="" /></dd>
+</dl>
+
+<dl>
+  <dt><label for='condition'>Condition:</label></dt>
+  <dd>
+    <select name='condition'>
+      <option value='new'>New</option>
+      <option value='likenew'>Like New</option>
+      <option value='good'>Good</option>
+      <option value='fair'>Fair</option>
+      <option value='poor'>Poor</option>
+    </select>
+  </dd>
+</dl>
+
+<dl>
+  <dt><label for='location'>Location:</label></dt>
+  <dd><input type="text" name="location" value="" /></dd>
+</dl>
