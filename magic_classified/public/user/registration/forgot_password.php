f15d67ecdcf137e09f95a733927c3ae2e3d89fb6
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 20:22:42 -0700

    Feat: added forgot_password.php

--- magic_classified/public/user/registration/forgot_password.php
+++ magic_classified/public/user/registration/forgot_password.php
@@ -1,5 +1,5 @@
-<?php require_once('../../private/initialize.php'); ?>
-
+<?php require_once('../../../private/initialize.php'); ?>
+<?php $page_title = 'Edit My Account'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <main class='row'>
@@ -8,29 +8,24 @@
   <article class='column listings'>
       <div class="listing_details">
         <div class='listing_title'>
-          <h2>Login to My Account</h2>
+          <h2>Forgot My Password</h2>
         </div><!-- .listing_title -->
 
         <div class='listing_body'>
           <div class='listing_info'>
             <form>
               <dl>
-                <dt><label for='username'>Username:</label></dt>
-                <dd><input type="text" name="username" value="" /></dd>
+                <dt><label for='email'>Email Address:</label></dt>
+                <dd><input type="text" name="email" value="" /></dd>
               </dl>
-              <dl>
-                <dt><label for='password'>Password:</label></dt>
-                <dd><input type="text" name="password" value="" /></dd>
-              </dl>
-              <input type='submit' value='Login' />
+              <input type='submit' value='Forgot Password' />
             </form>
           </div><!-- .listing_info -->
 
         </div><!--listing_body -->
         <div class='listing_footer'>
           <ul class='footer_menu'>
-            <li><a href='<?php echo url_for('/user/registration/forgot_password.php'); ?>'>Forgot my Password</a></li>
-            <li><a href='<?php echo url_for('/user/registration/new.php'); ?>'>Create Account</a></li>
+            <li><a href='<?php echo url_for('/user/login.php'); ?>'>Login</a></li>
           </ul>
         </div><!-- listing_footer -->
       </div><!-- listing_details -->
--- magic_classified/public/user/registration/new.php
+++ magic_classified/public/user/registration/new.php
@@ -1,5 +1,5 @@
-<?php require_once('../../private/initialize.php'); ?>
-
+<?php require_once('../../../private/initialize.php'); ?>
+<?php $page_title = 'Edit My Account'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <main class='row'>
@@ -8,29 +8,21 @@
   <article class='column listings'>
       <div class="listing_details">
         <div class='listing_title'>
-          <h2>Login to My Account</h2>
+          <h2>Create New Account</h2>
         </div><!-- .listing_title -->
 
         <div class='listing_body'>
           <div class='listing_info'>
             <form>
-              <dl>
-                <dt><label for='username'>Username:</label></dt>
-                <dd><input type="text" name="username" value="" /></dd>
-              </dl>
-              <dl>
-                <dt><label for='password'>Password:</label></dt>
-                <dd><input type="text" name="password" value="" /></dd>
-              </dl>
-              <input type='submit' value='Login' />
+              <?php include('form_fields.php'); ?>
+              <input type='submit' value='New account' />
             </form>
           </div><!-- .listing_info -->
 
         </div><!--listing_body -->
         <div class='listing_footer'>
           <ul class='footer_menu'>
-            <li><a href='<?php echo url_for('/user/registration/forgot_password.php'); ?>'>Forgot my Password</a></li>
-            <li><a href='<?php echo url_for('/user/registration/new.php'); ?>'>Create Account</a></li>
+            <li><a href='<?php echo url_for('/user/login.php'); ?>'>Login</a></li>
           </ul>
         </div><!-- listing_footer -->
       </div><!-- listing_details -->
