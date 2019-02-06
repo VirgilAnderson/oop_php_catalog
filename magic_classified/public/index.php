835d631203d53a25cac39924aa20cfda79c28015
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 23:10:05 -0700

    Refactor: Updated link to account on index.php

--- magic_classified/public/index.php
+++ magic_classified/public/index.php
@@ -5,7 +5,7 @@
 <main>
   <section id='main_menu'>
     <ul id='menu'>
-      <li><a href='<?php echo url_for('/user/login.php'); ?>'><i class="far fa-user-circle"></i> My Account</a></li>
+      <li><a href='<?php echo url_for('/user/registration/index.php'); ?>'><i class="far fa-user-circle"></i> My Account</a></li>
       <li><a href='<?php echo url_for('/listings.php'); ?>'><i class="fas fa-frog"></i> Listings</a></li>
     </ul>
   </section>
