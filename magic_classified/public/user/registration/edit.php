c09a87a8dc7a6b139c42fb29b6046e062724baf8
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 19:12:47 -0700

    Feat: Added template to registration/edit.php

---
+++ magic_classified/public/user/registration/edit.php
@@ -0,0 +1,11 @@
+<?php require_once('../../../private/initialize.php'); ?>
+<?php $page_title = 'Edit My Account'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<main>
+
+</main>
+
+
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
