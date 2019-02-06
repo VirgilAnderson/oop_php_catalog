608974a94689b1d4a911ac3ebbd75c7be7b55728
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 18:38:02 -0700

    Feat: Added more links to header.php instead of using user_header.php

--- magic_classified/public/user/index.php
+++ magic_classified/public/user/index.php
@@ -1,6 +1,6 @@
 <?php require_once('../../private/initialize.php'); ?>
 
-<?php include(SHARED_PATH . '/user_header.php'); ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
 
 <main>
   <section id='main_menu'>
