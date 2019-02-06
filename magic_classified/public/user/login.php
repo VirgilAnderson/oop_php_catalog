675b1c75655555414a517a59e277fd74e93b8a25
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-05 20:17:10 -0700

    Feat: Added form on login.php

--- magic_classified/public/user/login.php
+++ magic_classified/public/user/login.php
@@ -2,13 +2,42 @@
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
-<main>
-  <section id='main_menu'>
-    <ul id='menu'>
-      <li><a href='<?php echo url_for('/about.php'); ?>'>About Us</a></li>
-      <li><a href='<?php echo url_for('/listings.php'); ?>'>View Listings</a></li>
-    </ul>
-  </section>
+<main class='row'>
+  <aside class='column'><?php include(SHARED_PATH . '/sidebar.php'); ?></aside>
+
+  <article class='column listings'>
+      <div class="listing_details">
+        <div class='listing_title'>
+          <h2>Login to My Account</h2>
+        </div><!-- .listing_title -->
+
+        <div class='listing_body'>
+          <div class='listing_info'>
+            <form>
+              <dl>
+                <dt><label for='username'>Username:</label></dt>
+                <dd><input type="text" name="username" value="" /></dd>
+              </dl>
+              <dl>
+                <dt><label for='password'>Password:</label></dt>
+                <dd><input type="text" name="password" value="" /></dd>
+              </dl>
+              <input type='submit' value='Login' />
+            </form>
+          </div><!-- .listing_info -->
+
+        </div><!--listing_body -->
+        <div class='listing_footer'>
+          <ul class='footer_menu'>
+            <li><a href='<?php echo url_for('/user/registration/edit.php'); ?>'>Forgot my Password</a></li>
+            <li><a href='<?php echo url_for('/user/registration/delete.php'); ?>'>Create Account</a></li>
+          </ul>
+        </div><!-- listing_footer -->
+      </div><!-- listing_details -->
+  </article>
+
+  <aside class='column'>Column</aside>
+
 </main>
 
 
